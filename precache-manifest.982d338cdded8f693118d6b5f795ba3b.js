83a01b4eee6c4c538b3eacecbbb6b76633520d17
huykon <huykon@users.noreply.github.com>
2021-10-27 10:07:21 +0000

    deploy: 6e0009c344c333ab525f96fe781814c6849a6e80

---
+++ precache-manifest.982d338cdded8f693118d6b5f795ba3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46eacbee9153c49d729d899e9d0c2ca8",
+    "url": "https://huykon.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "741afde79b16aaffb3f6",
+    "url": "https://huykon.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "7bf1656e2fffe1aba944",
+    "url": "https://huykon.github.io/task-management-frontend/static/js/2.ef929c81.chunk.js"
+  },
+  {
+    "revision": "741afde79b16aaffb3f6",
+    "url": "https://huykon.github.io/task-management-frontend/static/js/main.69ae09ae.chunk.js"
+  },
+  {
+    "revision": "e3a0c7261274dcabfe07",
+    "url": "https://huykon.github.io/task-management-frontend/static/js/runtime~main.4f9ae2a1.js"
+  }
+]);
